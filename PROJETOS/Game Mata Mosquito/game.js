e18d775c6f520f154d2abfd6a057cfdfef124572
oth-gdy <othon.dev@outlook.com>
2019-01-22 22:26:14 -0200

    JAVASCRIPT

--- PROJETOS/Game Mata Mosquito/game.js
+++ PROJETOS/Game Mata Mosquito/game.js
@@ -1,5 +1,20 @@
 var altura = 0;
 var largura = 0;
+var vidas = 1;
+var tempo = 10;
+
+var intervalo = 1500;
+
+var nivel = window.location.search;
+nivel = nivel.replace("?","");
+
+if(nivel === "normal") {
+    intervalo = 1500;
+}else if (nivel === "dificil"){
+    intervalo = 1000;
+}else if(nivel === "chucknorris"){
+    intervalo = 750;
+}
 
 function define_palco(){
     altura = window.innerHeight;
@@ -10,7 +25,31 @@ function define_palco(){
 
 define_palco();
 
+var cronometro = setInterval(function(){
+    tempo -= 1;
+    
+    if(tempo < 0){
+        clearInterval(cronometro);
+        clearInterval(criaMosquito);
+        window.location.href = "vitoria.html";
+    }else{
+        document.getElementById('cronometro').innerHTML = tempo;
+    }
+
+},1000);
+
 function posicao_random(){
+    if(document.getElementById("mosquito")){
+        document.getElementById("mosquito").remove();
+        
+        if(vidas > 3){
+            window.location.href = "gameover.html";           
+        }else{
+            document.getElementById("vida"+vidas).src="img/coracao_vazio.png"; 
+            vidas++;
+        }        
+    }
+
     //foi feita a multiplicação pois o JavaScript gera valores aleatórios entre 0 e 1
     var posicaoX = Math.floor(Math.random() * largura) -90;
     var posicaoY = Math.floor(Math.random() * altura) -90;
@@ -26,8 +65,11 @@ function posicao_random(){
     mosquito.style.left = posicaoX + "px";
     mosquito.style.top = posicaoY + "px";
     mosquito.style.position = 'absolute';
-
-    document.body.appendChild(mosquito);  
+    mosquito.id = "mosquito"
+    document.body.appendChild(mosquito); 
+    mosquito.onclick = function(){
+        this.remove();
+    } 
     
     console.log(lado_random());
 }
